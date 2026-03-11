ef6a9ca5792a7d1f8f8fe0e9535e43d34b84a5ba
Taha-Khurram <tahakhurramofficial@gmail.com>
2026-03-12 02:12:38 +0500

    updated the retry analyzer

--- src/test/java/com/complya/framework/retry/RetryListener.java
+++ src/test/java/com/complya/framework/retry/RetryListener.java
@@ -12,8 +12,40 @@ public class RetryListener implements IAnnotationTransformer {
                           Class testClass,
                           Constructor testConstructor,
                           Method testMethod) {
-        if (annotation.getRetryAnalyzerClass() == null) {
-            annotation.setRetryAnalyzerClass(RetryAnalyzer.class);
+        if (getRetryAnalyzerClass(annotation) == null) {
+            setRetryAnalyzerClass(annotation, RetryAnalyzer.class);
+        }
+    }
+
+    private static Class<?> getRetryAnalyzerClass(ITestAnnotation annotation) {
+        try {
+            Method method = annotation.getClass().getMethod("getRetryAnalyzerClass");
+            return (Class<?>) method.invoke(annotation);
+        } catch (NoSuchMethodException ignored) {
+            try {
+                Method method = annotation.getClass().getMethod("getRetryAnalyzer");
+                return (Class<?>) method.invoke(annotation);
+            } catch (ReflectiveOperationException e) {
+                throw new IllegalStateException("Unable to read retry analyzer from ITestAnnotation", e);
+            }
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("Unable to read retry analyzer from ITestAnnotation", e);
+        }
+    }
+
+    private static void setRetryAnalyzerClass(ITestAnnotation annotation, Class<?> retryAnalyzerClass) {
+        try {
+            Method method = annotation.getClass().getMethod("setRetryAnalyzerClass", Class.class);
+            method.invoke(annotation, retryAnalyzerClass);
+        } catch (NoSuchMethodException ignored) {
+            try {
+                Method method = annotation.getClass().getMethod("setRetryAnalyzer", Class.class);
+                method.invoke(annotation, retryAnalyzerClass);
+            } catch (ReflectiveOperationException e) {
+                throw new IllegalStateException("Unable to set retry analyzer on ITestAnnotation", e);
+            }
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("Unable to set retry analyzer on ITestAnnotation", e);
         }
     }
 }
