e2fa49644af97dc1d20a3caa85dd542692e3703e
Taha-Khurram <tahakhurramofficial@gmail.com>
2026-03-12 01:56:10 +0500

    Added Book Demo page constructor

---
+++ src/test/java/com/complya/framework/pages/BookDemoPage.java
@@ -0,0 +1,11 @@
+package com.complya.framework.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BookDemoPage extends BasePage{
+
+    public BookDemoPage() { super(); }
+
+    public BookDemoPage(WebDriver driver){ super(driver); }
+
+}
